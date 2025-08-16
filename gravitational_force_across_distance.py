33e03987bd35a161b9f4741d78c188bc4744c81a
AmithMG <amithmg6@gmail.com>
2025-08-16 17:53:45 +0530

    chapter 2 completes

---
+++ gravitational_force_across_distance.py
@@ -0,0 +1,24 @@
+
+import matplotlib.pyplot as plt
+
+def draw_graph(x,y):
+    plt.plot(x,y,marker='o')
+    plt.title('Force vs Distance')
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Force (N)')
+    plt.show()
+
+def genrate_F_r():
+    r = range(100,1001,75)
+    F = []
+    G = 6.674*(10**-11)
+    m1 = 0.5
+    m2 = 1.5
+
+    for dist in r:
+        force = G*(m1*m2)/(dist**2)
+        F.append(force)
+    draw_graph(r, F)
+
+if __name__ == '__main__':
+    genrate_F_r()
